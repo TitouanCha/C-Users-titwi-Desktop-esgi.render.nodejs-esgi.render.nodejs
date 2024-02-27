0ebb1fadd06528aa3f2084536537a99cada66537
TitouanCha <150924281+TitouanCha@users.noreply.github.com>
2024-02-27 11:03:17 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
+  'process.env.DATABASE_NAME', // Database name
   '', // User
   '', // Password
   {
